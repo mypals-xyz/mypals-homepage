9d4b043ec0d729198d633a535d9953312595a63a
pals-ray <ray@mypals.xyz>
2024-03-26 22:02:04 +0900

    feat: add gif

--- src/components/BitFrame.tsx
+++ src/components/BitFrame.tsx
@@ -24,24 +24,16 @@ function borderClass(lrtb: LR | TB): string {
 
 export default function BitFrame({
   children,
-  borderColor,
+  borderColor
 }: Props): JSX.Element {
-  const [expand, setExpand] = useState<boolean>();
-  const size = "w-2 h-2";
-
-  const onHover = () => {
-    setExpand(true);
-  };
-
-  const onHoverLeave = () => {
-    setExpand(false);
-  };
+  const [inZone, setInZone] = useState<boolean>(false);
+  const [mouseDown, setMouseDown] = useState<boolean>(false);
+  const expand = inZone && !mouseDown;
 
   const calBorderClass = (lr: LR, tb: TB): string => {
     return cx(
-      "absolute",
+      "absolute w-2 h-2",
       borderColor,
-      size,
       borderClass(lr),
       borderClass(tb),
       // need add all classes will be used to tailwind safelist
@@ -49,7 +41,7 @@ export default function BitFrame({
         [`-${lr}-1`]: expand,
         [`-${tb}-1`]: expand,
         [`${lr}-0`]: !expand,
-        [`${tb}-0`]: !expand,
+        [`${tb}-0`]: !expand
       }
     );
   };
@@ -57,9 +49,13 @@ export default function BitFrame({
   return (
     <div
       className="relative inline-block"
-      onMouseOver={onHover}
-      onMouseLeave={onHoverLeave}
-    >
+      onMouseOver={() => setInZone(true)}
+      onMouseLeave={() => {
+        setInZone(false);
+        setMouseDown(false);
+      }}
+      onMouseDown={() => setMouseDown(true)}
+      onMouseUp={() => setMouseDown(false)}>
       <div className={calBorderClass("left", "top")} />
       <div className={calBorderClass("left", "bottom")} />
       <div className={calBorderClass("right", "top")} />
